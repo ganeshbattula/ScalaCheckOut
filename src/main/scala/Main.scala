d5bcbe548133a24feffc2274f3a4830d55b9509e
Ganesh Battula <ganesh990877@gmail.com>
2024-07-02 14:04:55 +0100

    Implemented checkout system with offers

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -10,9 +10,20 @@ object Main {
     BigDecimal(total).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
   }
 
+  def calculateTotalWithOffers(items: List[String]): Double = {
+    val appleCount = items.count(_ == "Apple")
+    val orangeCount = items.count(_ == "Orange")
+
+    val appleCost = ((appleCount / 2) + (appleCount % 2)) * applePrice
+    val orangeCost = ((orangeCount / 3) * 2 + (orangeCount % 3)) * orangePrice
+
+    val total = appleCost + orangeCost
+    BigDecimal(total).setScale(2, BigDecimal.RoundingMode.HALF_UP).toDouble
+  }
+
   def main(args: Array[String]): Unit = {
-    val items = List("Apple", "Apple", "Orange", "Apple")
-    val total = calculateTotal(items)
-    println(s"Total cost: £$total")
+    val items = List("Apple", "Apple", "Orange", "Apple", "Orange", "Orange", "Orange")
+    val total = calculateTotalWithOffers(items)
+    println(s"Total cost with offers: £$total")
   }
 }
